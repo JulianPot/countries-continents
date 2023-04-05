f9deb831eb9de54d9c134e53b852d044a6fda657
JulianPot <julek.w.p@gmail.com>
2023-04-05 22:56:50 +0200

    css

--- countries.js
+++ countries.js
@@ -1,14 +1,15 @@
 const getCountries = async () => {
-  document.getElementById("countries").innerText = "loading....";
-
   const continent = document.getElementById("continents").value;
   let countriesDisplayNum = document.getElementById("quantity").value;
   let continentID;
 
   if (countriesDisplayNum < 2 || countriesDisplayNum > 10) {
     alert("Wrong number of countries to display!");
+    document.getElementById("countries").innerText = "";
     return;
   }
+
+  document.getElementById("countries").innerText = "loading....";
 
   if (continent === "Africa") {
     continentID = 0;
@@ -47,7 +48,7 @@ const getCountries = async () => {
 
     if (countriesArray.length < countriesDisplayNum) {
       countriesDisplayNum = countriesArray.length;
-      alert("There is less countries than you want to display!")
+      alert("There is less countries than you want to display!");
     }
 
     document.getElementById("countries").innerText = "";
@@ -69,33 +70,57 @@ const getCountries = async () => {
 };
 
 const getCountryDetails = async (countryName) => {
-    document.getElementById(countryName).innerText = "loading details....";
+  document.getElementById(countryName).innerText = "loading details....";
 
-    try {
-        const result = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
-        const countryData = await result.json();
+  try {
+    const result = await fetch(
+      `https://restcountries.com/v3.1/name/${countryName}`
+    );
+    const countryData = await result.json();
 
-        const getDataWithoutKey = (data) => {
-            let object = data;
-            for (let key in object) {
-                if (object.hasOwnProperty(key)) {
-                    return object[key];
-                }
-            }
+    const getDataWithoutKey = (data) => {
+      let object = data;
+      for (let key in object) {
+        if (object.hasOwnProperty(key)) {
+          return object[key];
         }
-
-        document.getElementById(countryName).innerHTML = `
-        <h3>${countryName}</h3>
-        <p>Official name: ${typeof(countryData[0].name.official) === "string" ? countryData[0].name.official : "No information found!"}</p>
-        <p>Capital: ${typeof(countryData[0].capital) === "object" ? countryData[0].capital : "No information found!"}</p>
-        <p>Population: ${typeof(countryData[0].population) === "number" ? countryData[0].population : "No information found!"}</p>
-        <p>Currency: ${typeof(getDataWithoutKey(countryData[0].currencies).name) === "string" ? getDataWithoutKey(countryData[0].currencies).name : "No information found!"}</p>
-        <p>Subregion: ${typeof(countryData[0].subregion) === "string" ? countryData[0].subregion : "No information found!"}</p>
-        <p>Languages: ${typeof(getDataWithoutKey(countryData[0].languages)) === "string" ? getDataWithoutKey(countryData[0].languages) : "No information found!"}</p>
-        `;
-
-      } catch (err) {
-        console.log(err);
       }
-}
-{/* ${typeof(getDataWithoutKey(countryData[0].languages)) === "string" ? getDataWithoutKey(countryData[0].languages) : "No information found!"} */}
+    };
+
+    document.getElementById(countryName).innerHTML = `
+        <h3>${countryName}</h3>
+        <p>Official name: ${
+          typeof countryData[0].name.official === "string"
+            ? countryData[0].name.official
+            : "No information found!"
+        }</p>
+        <p>Capital: ${
+          typeof countryData[0].capital === "object"
+            ? countryData[0].capital
+            : "No information found!"
+        }</p>
+        <p>Population: ${
+          typeof countryData[0].population === "number"
+            ? countryData[0].population
+            : "No information found!"
+        }</p>
+        <p>Currency: ${
+          typeof getDataWithoutKey(countryData[0].currencies).name === "string"
+            ? getDataWithoutKey(countryData[0].currencies).name
+            : "No information found!"
+        }</p>
+        <p>Subregion: ${
+          typeof countryData[0].subregion === "string"
+            ? countryData[0].subregion
+            : "No information found!"
+        }</p>
+        <p>Languages: ${
+          typeof getDataWithoutKey(countryData[0].languages) === "string"
+            ? getDataWithoutKey(countryData[0].languages)
+            : "No information found!"
+        }</p>
+        `;
+  } catch (err) {
+    console.log(err);
+  }
+};
